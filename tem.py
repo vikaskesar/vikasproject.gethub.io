e0e672c504e44d7ea437c8aeace7173befc5e877
vikaskesar <144890383+vikaskesar@users.noreply.github.com>
2023-09-15 11:41:32 +0530

    How to temprature

---
+++ tem.py
@@ -0,0 +1,8 @@
+from tkinter import*
+win=Tk()
+win.geometry('600x300')
+win.title('Temprature ')
+
+#Lable
+#lbln1=Label (win,text='TEMPRATURE :- ',fg='blue',font=('Arial',20))
+#lbln1.place (x=150,y=20)
